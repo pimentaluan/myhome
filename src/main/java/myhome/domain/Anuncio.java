f48c94a8c21fb5da784e2b2565cfaff8eab18ee8
Laila Aquino <lailaaquino8@gmail.com>
2026-01-31 14:21:00 -0300

    altera toString

--- src/main/java/myhome/domain/Anuncio.java
+++ src/main/java/myhome/domain/Anuncio.java
@@ -5,22 +5,20 @@ import java.util.UUID;
 public class Anuncio {
     private final String id = UUID.randomUUID().toString();
     private final String titulo;
-    private final double preco;
     private final Imovel imovel;
     private final Anunciante anunciante;
 
     private AnuncioStatus status = AnuncioStatus.RASCUNHO;
 
-    public Anuncio(String titulo, double preco, Imovel imovel, Anunciante anunciante) {
+    public Anuncio(String titulo, Imovel imovel, Anunciante anunciante) {
         this.titulo = titulo;
-        this.preco = preco;
         this.imovel = imovel;
         this.anunciante = anunciante;
     }
 
     public String getId() { return id; }
     public String getTitulo() { return titulo; }
-    public double getPreco() { return preco; }
+    public double getPreco() { return imovel.preco(); }
     public Imovel getImovel() { return imovel; }
     public Anunciante getAnunciante() { return anunciante; }
     public AnuncioStatus getStatus() { return status; }
@@ -31,12 +29,14 @@ public class Anuncio {
 
     @Override
     public String toString() {
-        return "Anuncio{id=" + id +
-                ", titulo='" + titulo + '\'' +
-                ", preco=" + preco +
-                ", imovel=" + imovel.tipo() + "(" + imovel.resumo() + ")" +
-                ", anunciante=" + anunciante.getNome() +
-                ", status=" + status +
-                '}';
+        return "Anuncio{\n id=" + id +
+                ",\n titulo='" + titulo + '\'' +
+                ",\n preco=" + getPreco() +
+                ",\n imovel=" + imovel.tipo() +
+                "(área=" +imovel.area() + ", " + imovel.resumo() + ")" +
+                ",\n anunciante=" + anunciante.getNome() +
+                ",\n status=" + status +
+                "\n} " +
+                "\n--------------------------------------------";
     }
 }
